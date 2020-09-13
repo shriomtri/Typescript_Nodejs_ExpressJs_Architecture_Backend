4774717af9f3fe22e0e4db29eb3cf2b7f9b1d74f
shriom <blackclover@mail.gmail.com>
2020-09-14 02:05:03 +0530

    added apierror handler and api response handler

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { corsUrl, environment } from './config';
 // import './database'; // initialize database
-// import { NotFoundError, ApiError, InternalError } from './core/ApiError';
+import { NotFoundError, ApiError, InternalError } from './core/ApiError';
 // import routesV1 from './routes/v1';
 
 process.on('uncaughtException', (e) => {
@@ -21,20 +21,20 @@ app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 // app.use('/v1', routesV1);
 
 // catch 404 and forward to error handler
-// app.use((req, res, next) => next(new NotFoundError()));
+app.use((req, res, next) => next(new NotFoundError()));
 
-// Middleware Error Handler
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-//   if (err instanceof ApiError) {
-//     ApiError.handle(err, res);
-//   } else {
-//     if (environment === 'development') {
-//       Logger.error(err);
-//       return res.status(500).send(err.message);
-//     }
-//     ApiError.handle(new InternalError(), res);
-//   }
-// });
+//Middleware Error Handler
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    ApiError.handle(err, res);
+  } else {
+    if (environment === 'development') {
+      Logger.error(err);
+      return res.status(500).send(err.message);
+    }
+    ApiError.handle(new InternalError(), res);
+  }
+});
 
 export default app;
